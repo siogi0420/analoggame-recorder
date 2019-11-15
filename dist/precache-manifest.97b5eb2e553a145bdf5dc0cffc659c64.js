dd67d75cb57f808228bbb4f5ca74556dc56afacc
siogi0420 <siogi0420@gmail.com>
2019-11-16 00:33:15 +0900

    add dist

---
+++ dist/precache-manifest.97b5eb2e553a145bdf5dc0cffc659c64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e3970bb78537dcfb9b",
+    "url": "/css/app.d5771a2f.css"
+  },
+  {
+    "revision": "425a8087e5681753c0b8",
+    "url": "/css/chunk-vendors.622856b7.css"
+  },
+  {
+    "revision": "0c5d7d9b669086c81ce28803954a0210",
+    "url": "/img/logo.0c5d7d9b.png"
+  },
+  {
+    "revision": "de3feb473ffff33554d48e091dcabda5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53e3970bb78537dcfb9b",
+    "url": "/js/app.48dd8008.js"
+  },
+  {
+    "revision": "425a8087e5681753c0b8",
+    "url": "/js/chunk-vendors.7221268a.js"
+  },
+  {
+    "revision": "481a9f0789c131d08a071e8a492138c5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
